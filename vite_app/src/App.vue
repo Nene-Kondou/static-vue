54ee3110a1fefac0f9419b9a3fab464831eaddbb
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-05-04 22:42:04 +0900

    vite vue component

--- vite_app/src/App.vue
+++ vite_app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div id="app">
+    <HelloWorld title="Hello" />
+  </div>
 </template>
 
 <script>
