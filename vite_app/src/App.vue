ab08f1db19033d7184b0831554f73f15bc430ed3
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-05-05 11:33:23 +0900

    親子コンポーネント間の値の受け渡し

--- vite_app/src/App.vue
+++ vite_app/src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <HelloWorld title="Hello" />
+    <!-- ① -->
+    <!-- <HelloWorld title="Hello" /> -->
+
+    <!-- ②親コンポーネントから子コンポーネントへ値を渡す タイトルを変更 -->
+    <!-- <HelloWorld v-bind:title="message" />
+    <hr>
+    <button class="btn btn-primary" v-on:click="doAction">change title</button> -->
+
+    <!-- ③子コンポーネントから親コンポーネントへ -->
+    <HelloWorld v-bind:title="message" v-on:result-event="appAction" />
+    <hr>
+    <p>{{ result }}</p>
+
+    <!-- ④テンプレート参照 -->
   </div>
 </template>
 
@@ -11,6 +24,32 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+
+  // ②
+  // data() {
+  //   return {
+  //     message: 'HELLO',
+  //   }
+  // },
+  // methods: {
+  //   doAction() {
+  //     var input = prompt( "new title:" )
+  //     this.message = input
+  //   }
+  // },
+
+  // ③
+  data() {
+    return {
+      message: 'HELLO',
+      result: 'no event',
+    }
+  },
+  methods: {
+    appAction( message ) {
+      this.result = '(*** you send:"' + message + '". ***)'
+    }
+  },
 }
 </script>
--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -1,32 +1,61 @@
 <template>
   <div class="alert alert-info">
-    <h2>{{ title }}</h2>
-    <p>{{ message }}</p>
+    <!-- <h2>{{ title }}</h2>
+    <p>{{ message }}</p> -->
+
     <!-- ②v-onによるイベントの利用 -->
-    <hr>
+    <!-- <hr>
     <div>
       <input type="tel" class="form-control" v-model="input">
       <button class="btn btn-info mt-2" v-on:click="doAction">Click</button>
-    </div>
+    </div> -->
+    <!-- ④テンプレート参照 -->
+    <h1>{{ title }}</h1>
+    <p ref="msg">{{ message }}</p>
+    <button class="btn btn-primary" v-on:click="DoAction">Click</button>
+
   </div>
+
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    title: String,
-    message: String,
-  },
+  // ①②③
+  // props: {
+  //   title: String,
+  //   message: String,
+  // },
+  // data() {
+  //   return {
+  //     message: 'お名前は？',
+  //     input: 'no name',
+  //   }
+  // },
+  // methods: {
+  //   doAction() {
+  //     // ①
+  //     this.message = 'こんにちは、' + this.input + 'さん！'
+  //     // ②
+  //     this.$emit( 'result-event', this.input )
+  //   }
+  // },
+
+  // ④
   data() {
     return {
-      message: 'お名前は？',
-      input: 'no name',
+      title: 'HelloWorld',
+      message: 'This is sample message.'
     }
   },
+  mounted() {
+    this.counter = 0
+  },
   methods: {
-    doAction() {
-      this.message = 'こんにちは、' + this.input + 'さん！'
+    DoAction() {
+      // pタグにh6タグを追加している
+      this.counter++
+      this.$refs.msg.innerHTML += '<h6>counted: ' + this.counter + '</h6>'
     }
   },
 }
