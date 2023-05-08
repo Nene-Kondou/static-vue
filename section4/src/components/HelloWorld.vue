f81a9f37ce42e3327be0fbbf06e0044af5b4e301
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-05-09 07:56:57 +0900

    プロパティのバリデーション

---
+++ section4/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="alert alert-primary">
+
+    <!-- ① -->
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+    <hr>
+    <p>Number: {{ num }}</p> 
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+    // ①
+    // num: Number,
+
+    // ②
+    num: {
+      type: Number,
+      default: 100,
+      validator: ( value ) => {
+        // 3つの式が全部trueならtrue 整数で0以上100未満
+        return value == parseInt( value ) && value >= 0 && value <= 100
+      },
+    }
+  },
+  data() {
+    return {
+      message: 'バリデーション・チェック',
+    }
+  }
+}
+</script>
