635d0f71e9825260ced2d9f944eedae501eac3ec
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-05-11 05:52:39 +0900

    ウォッチャ

--- section4/src/components/HelloWorld.vue
+++ section4/src/components/HelloWorld.vue
@@ -8,20 +8,36 @@
     <p>Number: {{ num }}</p>  -->
     
     <!-- ④getter setter -->
+    <!-- <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+    <hr>
+    <p class="h5">val: {{ val }}</p> -->
+    <!-- aを入力するとbは、aを2で割って2条した数字 -->
+    <!-- <div class="form-group text-left">
+      <label for="">* 2:</label>
+      <input type="number" v-model="a" class="form-control">
+    </div> -->
+    <!-- bを入力するとah、bの平方根の2倍 -->
+    <!-- <div class="form-group text-left">
+      <label for="">^ 2:</label>
+      <input type="number" v-model="b" class="form-control">
+    </div> -->
+
+    <!-- ⑤ウォッチャ 四則演算の結果を監視する -->
     <h2>{{ title }}</h2>
     <p>{{ message }}</p>
     <hr>
-    <p class="h5">val: {{ val }}</p>
-    <!-- aを入力するとbは、aを2で割って2条した数字 -->
     <div class="form-group text-left">
-      <label for="">* 2:</label>
-      <input type="number" v-model="a" class="form-control">
+      <label>Value:</label>
+      <input type="number" v-model="val" class="gorm-control">
     </div>
-    <!-- bを入力するとah、bの平方根の2倍 -->
-    <div class="form-group text-left">
-      <label for="">^ 2:</label>
-      <input type="number" v-model="b" class="form-control">
-    </div>
+    <table class="bg-white table mt-4">
+      <tr><th>add:</th><td>{{ add }}</td></tr>
+      <tr><th>sub:</th><td>{{ sub }}</td></tr>
+      <tr><th>multiple:</th><td>{{ multiple }}</td></tr>
+      <tr><th>divide:</th><td>{{ divide }}</td></tr>
+    </table>
+
   </div>
 
 </template>
@@ -45,7 +61,7 @@ export default {
     // }
   },
 
-  //   // ②
+  //   // ③
   // data() {
     // return {
     //   message: 'バリデーション・チェック',
@@ -53,33 +69,60 @@ export default {
     
   // },
 
-  // ③
+  // ④
+  // data() {
+  //   return {
+  //     message: '算術プロパティの利用。',
+  //     val: 0,
+  //   }
+  // },
+  // computed: {
+  //   a: {
+  //     get() {
+  //       return this.val * 2
+  //     },
+  //     set( value ) {
+  //       this.val = Math.floor( value / 2 )
+  //     },
+  //   },
+  //   b: {
+  //     get() {
+  //       return this.val * this.val
+  //     },
+  //     set( value ) {
+  //       this.val = Math.floor( Math.sqrt( value ))
+  //     },
+  //   },
+  // },
+  // created() {
+  //   this.val = 10
+  // },
+
+  // ⑤
   data() {
     return {
-      message: '算術プロパティの利用。',
+      title: 'Watchers',
+      message: '値の監視',
       val: 0,
+      add: 0,
+      sub: 0,
+      multiple: 0,
+      divide: 0,
     }
   },
-  computed: {
-    a: {
-      get() {
-        return this.val * 2
-      },
-      set( value ) {
-        this.val = Math.floor( value / 2 )
-      },
-    },
-    b: {
-      get() {
-        return this.val * this.val
-      },
-      set( value ) {
-        this.val = Math.floor( Math.sqrt( value ))
-      },
-    },
+  watch: {
+    val( newVal, oldVal ) {
+      console.log( oldVal + ' -> ' + newVal )
+      this.val = newVal
+      var val = parseInt( this.val ) //値を整数に
+      this.add = Math.floor( val + 2 )
+      this.sub = Math.floor( val - 2 )
+      this.multiple = Math.floor( val * 2 )
+      this.divide = Math.floor( val / 2 )
+    }
   },
   created() {
-    this.val = 10
+    this.val = 6
   },
 }
 </script>
