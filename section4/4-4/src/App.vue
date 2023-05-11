0166eaca606d6542f9b8de8173b33a448c8fb397
近藤音々 <kondouoto@kondouotonoMacBook-Air.local>
2023-05-12 06:20:48 +0900

    slot

---
+++ section4/4-4/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <!-- ①slot -->
+    <!-- <HelloWorld title="slot">
+      <p>***this is defalut text***</p>
+    </HelloWorld> -->
+
+    <!-- ②名前付きスロット -->
+    <!-- <HelloWorld>
+      <p>Begin!..</p>
+      <p>..Middle..</p>
+      <p>..End</p>
+      <template v-slot:first>
+        ***First message***
+      </template>
+      <template v-slot:second>
+        ***Second message***
+      </template>
+      <template v-slot:third>
+        ***Third message***
+      </template>
+    </HelloWorld> -->
+
+    <!-- ③スロットに値を設定する 親コンポーネント側の値を表示 -->
+    <HelloWorld title="slot">
+      <li class="list-group-item" v-for="obj in slotobjs" v-bind:key="obj.name">
+        {{ obj.name }} {{ obj.mail }}
+      </li>
+    </HelloWorld>  
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  // ①②
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  // ③
+  data() {
+    return {
+      slotobjs: [
+        { name: 'Taro', mail: 'taro@yamada'},
+        { name: 'Hanako', mail: 'hanako@flower'},
+        { name: 'Sachiko', mail: 'sachiko@happy'},
+      ],
+    }
+  },
+}
+</script>
---
+++ section4/4-4/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="alert alert-primary">
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+    <hr>
+    <div class="alert alert-light">
+      <!-- ①slot -->
+      <!-- <h3>Inner Slot</h3>
+      <slot />  -->
+      
+      <!-- ②名前付きスロットを使う -->
+      <!-- <h3>Inner Slot</h3>
+      <div class="alert alert-warning h5">
+        <slot />
+      </div>
+      <ul class="list-group">
+        <li class="list-group-item">
+          <slot name="first" />
+        </li>
+        <li class="list-group-item">
+          <slot name="second" />
+        </li>
+        <li class="list-group-item">
+          <slot name="third" />
+        </li>
+      </ul> -->
+
+      <!-- ③スロットに値を設定する -->
+      <h3>Inner Slot</h3>
+      <ol class="list-group text-left">
+        <slot />
+      </ol>
+    </div>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data: function() {
+    return {
+      title: 'Slot',
+      message: 'This is message.',
+    }
+  },
+}
+</script>
